392b0c4df9d47bb15b100bee0fca465c78fc4c05
egitelma <egitelma@ucsc.edu>
2024-05-06 17:52:18 -0700

    setup main.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+let config = {
+    type: Phaser.WEBGL,
+    width: 1280,
+    height: 1280,
+    render: {
+        pixelArt: true
+    },
+    scene: [Load, Menu, Play, End],
+    backgroundColor: "#FFFFFF",
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+}
+
+let game = new Phaser.Game(config);
+
+let { width, height } = game.config;
