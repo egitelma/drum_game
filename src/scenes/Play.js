2c90353d37832976cf14552b06db8abb9e9e55a5
egitelma <egitelma@ucsc.edu>
2024-05-08 16:47:33 -0700

    moved assets folder, added (non-functional) punch tweens

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,18 +1,10 @@
 class Play extends Phaser.Scene {
 	constructor() {
-		super("Play");
+		super("playScene");
 	}
 
 	preload() {
-		//this.load.image("background",    "assets/background.png");
-		//this.load.image("playerFistL",   "assets/playerFistL.png");
-		//this.load.image("playerFistR",   "assets/playerFistR.png");
-		//this.load.image("enemy",         "assets/enemy.png");
-		//this.load.image("drum",          "assets/drum.png);
-		//this.load.image("inputL",        "assets/inputL.png");
-		//this.load.image("inputR",        "assets/inputR.png");
-		//this.load.image("inputDodgeL",   "assets/inputDodgeL.png");
-		//this.load.image("inputDodgeR",   "assets/inputDodgeR.png");
+		
 	}
 
 	create() {
@@ -24,17 +16,18 @@ class Play extends Phaser.Scene {
 		this.inputLockedOut = false;
 
 		//Health UI - two rectangles, on top left and top right, both red
-		this.add.rectangle(0, 0, 100, 20, 0xFF0000).setOrigin(0, 0);
-		this.add.rectangle(700, 0, 100, 20, 0xFF0000).setOrigin(0, 0);
+		this.add.rectangle(0, 0, 400, 20, 0xFF0000).setOrigin(0, 0);
+		this.add.rectangle(width-400, 0, 400, 20, 0xFF0000).setOrigin(0, 0);
 
 		//Time UI - top center, white
-		this.timeRemainingText = this.add.text(400, 0, "Time: " + timeRemaining, { fontFamily: "Arial", fontSize: "20px", color: "#FFFFFF" }).setOrigin(0.5, 0);
+		this.timeRemainingText = this.add.text(width/2-64, 0, "Time: " + this.timeRemaining, { fontFamily: "Arial", fontSize: "20px", color: "#FFFFFF" }).setOrigin(0.5, 0);
 
 		//Player Left Fist
-		//this.add.image(100, 300, "playerFistL").setOrigin(0, 0);
-
-		//Player Right Fist
-		//this.add.image(300, 300, "playerFistR").setOrigin(0, 0);
+		this.leftFist = this.add.image(0, 300, "fistLeft").setOrigin(0, 0);
+        this.leftFist.setScale(0.25);
+		//Player Right Fist (img dimensions: 1632x1224 so 0.25x the width is 408)
+		this.rightFist = this.add.image(width-408, 300, "fistRight").setOrigin(0, 0);
+        this.rightFist.setScale(0.25);
 
 		//Enemy - middle slightly above center
 		//this.add.image(400, 200, "enemy").setOrigin(0, 0);
@@ -46,10 +39,10 @@ class Play extends Phaser.Scene {
 		//use taiko no tatsujin drum icons? switch between white and red/blue for input off and on
 
 		//Inputs
-        keyLEFTPUNCH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        keyRIGHTPUNCH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
-        keyLEFTDODGE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-		keyRIGHTDODGE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+        this.keyLEFTPUNCH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyRIGHTPUNCH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+        this.keyLEFTDODGE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+		this.keyRIGHTDODGE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
 	}
 
 	update() {
@@ -64,19 +57,43 @@ class Play extends Phaser.Scene {
 			//change health bar size based on health
 
 			//Dodging
-			if (Phaser.Input.Keyboard.JustDown(keyRIGHTDODGE) && !inputLockedOut) { //Player Right Dodge
+			if (Phaser.Input.Keyboard.JustDown(this.keyRIGHTDODGE) && !this.inputLockedOut) { //Player Right Dodge
 				//handle right dodge movement
 			}
-			if (Phaser.Input.Keyboard.JustDown(keyLEFTDODGE) && !inputLockedOut) { //Player Left Dodge
+			if (Phaser.Input.Keyboard.JustDown(this.keyLEFTDODGE) && !this.inputLockedOut) { //Player Left Dodge
 				//handle left dodge movement
 			}
 
 			//Punching
-			if (Phaser.Input.Keyboard.JustDown(keyRIGHTPUNCH) && !inputLockedOut) { //Player Right Punch
+			if (Phaser.Input.Keyboard.JustDown(this.keyRIGHTPUNCH) && !this.inputLockedOut) { //Player Right Punch
 				//play right punch animation - maybe just like move fist sprite towards enemy sprite and make it a bit smaller
+                console.log("right punch");
+                this.tweens.add({
+                    targets: this.fistLeft,
+                    ease: "Bounce.easeIn",
+                    paused: true,
+                    yoyo: false,
+                    angle: {
+                        from: 0,
+                        to: -45,
+                        duration: 500
+                    }
+                }).play();
 			}
-			if (Phaser.Input.Keyboard.JustDown(keyLEFTPUNCH) && !inputLockedOut) { //Player Left Punch
+			if (Phaser.Input.Keyboard.JustDown(this.keyLEFTPUNCH) && !this.inputLockedOut) { //Player Left Punch
 				//play left punch animation - maybe just like move fist sprite towards enemy sprite and make it a bit smaller
+                console.log("left punch");
+                this.tweens.add({
+                    targets: this.fistRight,
+                    ease: "Bounce.easeIn",
+                    paused: true,
+                    yoyo: false,
+                    angle: {
+                        from: 0,
+                        to: 45,
+                        duration: 500
+                    }
+                }).play();
 			}
 			
 			//Enemy AI
